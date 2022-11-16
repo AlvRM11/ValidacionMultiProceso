7eaff91969d7cb411c1810f8ce46951368fe0774
Álvaro Ramos <78912452+AlvRM11@users.noreply.github.com>
2022-11-16 13:36:50 +0000

    Add files via upload

--- validacionMultiProceso.py
+++ validacionMultiProceso.py
@@ -3,22 +3,19 @@ from time import sleep
 from datetime import datetime
 
 def padre():
-    try:
+    for i in range(10):
 
-        for i in range(10):
+        newpid = os.fork()
 
-            newpid = os.fork()
+        if newpid == 0:
+            
+            horaActual = datetime.now().time().strftime("%H:%M:%S")
 
-            if newpid == 0:
-                horaActual = datetime.now().time().strftime("%H:%M:%S")
+            print("\n>><< Iniciando el proceso: {} a las {}".format(os.getpid(), horaActual))
 
-                print("\n>><< Iniciando el proceso: {} a las {}".format(os.getpid(), horaActual))
-                hijo()
+            hijo()
 
-            sleep(10)
-
-    except:
-        print("Ha ocurrido un error, inténtelo más tarde")
+        sleep(10)
 
 
 def hijo():
